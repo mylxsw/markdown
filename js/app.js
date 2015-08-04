fb12e15e812b633b14e83b1d48b3066ee63fb893
管宜尧 <mylxsw@126.com>
2015-08-04 11:53:05 +0800

    udpate

---
+++ js/app.js
@@ -0,0 +1,40 @@
+
+var Editor = React.createClass({displayName: "Editor",
+    getInitialState: function() {
+        return {value: 'Hello'};
+    },
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+    handleKeydown: function(event) {
+        console.log(event.which);
+        if (event.which == 9) {
+            // Tab按键处理
+            event.preventDefault();
+
+            var val = event.target.value,
+                start = event.target.selectionStart,
+                end = event.target.selectionEnd;
+
+            event.target.value = val.substring(0, start) + '\t' + val.substring(end);
+
+            event.target.selectionStart = event.target.selectionEnd = start + 1;
+        }
+    },
+    render: function() {
+        return (
+            React.createElement("div", {className: "editor-groups"}, 
+                React.createElement("div", {className: "editor"}, 
+                    React.createElement("textarea", {onChange: this.handleChange, onKeyDown: this.handleKeydown, ref: "editor", value: this.state.value})
+                ), 
+                React.createElement("div", {className: "preview", dangerouslySetInnerHTML: {__html: marked(this.state.value, {sanitize: true})}})
+            )
+        );
+    }
+});
+
+
+React.render(
+    React.createElement(Editor, null),
+    document.getElementById('editor')
+);
---
+++ src/app.js
@@ -0,0 +1,40 @@
+
+var Editor = React.createClass({
+    getInitialState: function() {
+        return {value: 'Hello'};
+    },
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
+    handleKeydown: function(event) {
+        console.log(event.which);
+        if (event.which == 9) {
+            // Tab按键处理
+            event.preventDefault();
+
+            var val = event.target.value,
+                start = event.target.selectionStart,
+                end = event.target.selectionEnd;
+
+            event.target.value = val.substring(0, start) + '\t' + val.substring(end);
+
+            event.target.selectionStart = event.target.selectionEnd = start + 1;
+        }
+    },
+    render: function() {
+        return (
+            <div className="editor-groups">
+                <div className="editor">
+                    <textarea onChange={this.handleChange} onKeyDown={this.handleKeydown} ref="editor" value={this.state.value} />
+                </div>
+                <div className="preview" dangerouslySetInnerHTML={{__html: marked(this.state.value, {sanitize: true})}} />
+            </div>
+        );
+    }
+});
+
+
+React.render(
+    <Editor />,
+    document.getElementById('editor')
+);
